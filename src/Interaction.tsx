7639d3e2d61210c648392f8aabb5b3471a226b36
Quentin Guenther <qguenther@outlook.com>
2019-09-22 23:22:22 -0700

    add Interaction panel

---
+++ src/Interaction.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface InteractionProps {
+  comments: number;
+  votes: number;
+}
+
+export function Interaction (props: InteractionProps) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
